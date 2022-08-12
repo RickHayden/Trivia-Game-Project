c4ebf152dfdc9fccaa78ed2f5c2376dcd482b9fe
RickHayden <richard.hayden459@live.com>
2022-08-12 04:27:04 -0400

    Removed excess code, win conditions bearable but may be polished more

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//console.log('LINK: START')
 // grab elements from html
 let questions = document.querySelectorAll('.questions')
 let answer = document.querySelectorAll('.answer')
 let submit = document.querySelector('.submit')
 
-// make submit button record and respond to answers from radio input
+// make submit button recognize and respond to answers from radio input
 function handleClick() {
     const radioButtons = document.querySelectorAll('input[value= "4"]');
     let selectedValue;
@@ -17,10 +16,7 @@ function handleClick() {
     if (selectedValue === '4')  {
         alert('Great job! Press the "Next" button to move on to the next branch, climber!');
       }
-      else if (selectedValue === '8') {
-        alert('Congratulations! you made it to the top of the tree. Press "next" to enjoy the view!');
-      }
-    else{
-        alert("Ahh, you almost had it... Maybe? Oh well. Try again!");
+    else {
+        alert("Ah, you almost had it... Oh well. Try again!");
     }
     }
