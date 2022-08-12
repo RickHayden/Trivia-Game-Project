250663616f3b00668cf342c2e11119c239ab81dc
RickHayden <richard.hayden459@live.com>
2022-08-12 00:25:45 -0400

    Major design changes, added Start page etc.

--- app.js
+++ app.js
@@ -1,67 +1,102 @@
-console.log('LINK: START')
-// let gameActive = true;
+//console.log('LINK: START')
+// grab elements from html
+let gameActive = true;
 let questions = document.querySelectorAll('.questions')
-// let answerA = document.querySelector('.AnswerA')
-// let answerB = document.querySelector('.AnswerB')
-// let answerC = document.querySelector('.AnswerC')
-// let answerD = document.querySelector('.AnswerD')
 let startButton = document.querySelector('#startButton')
-console.log(questions)
-
-// hide questions until game starts
-questions[0].style.visibility = 'hidden'
-questions[1].style.visibility = 'hidden'
-questions[2].style.visibility = 'hidden'
-questions[3].style.visibility = 'hidden'
-questions[4].style.visibility = 'hidden'
+let answer = document.querySelectorAll('.answer')
+let nextButton = document.querySelector('.next')
+let part2 = document.querySelectorAll(".questions2")
 
 
-// Have start button show questions
-startButton.addEventListener('click', function (event){
-    questions[0].style.visibility = 'block';
-    questions[1].style.visibility = 'block';
-    questions[2].style.visibility = 'block';
-    questions[3].style.visibility = 'block';
-    questions[4].style.visibility = 'block';
+
+
+
+
+
+// questions[0].style.visibility = 'hidden';
+// questions[1].style.visibility = 'hidden';
+// questions[2].style.visibility = 'hidden';
+// questions[3].style.visibility = 'hidden';
+// questions[4].style.visibility = 'hidden';
+
+
+
+startButton.addEventListener('click', function(evt) {
+    questions.forEach(function(e) {
+        questions.style.display = 'block'
+    })
+    gameActive = true;
 })
 
-// Configure Timer along with same start button as game
-// const timer = document.getElementById('timer');
-// let timerInterval
 
 
-// startTimer = () => {
-//     // Firs twe start by clearing the existing timer, in case of a restart
-//     clearInterval(timerInterval);
-//     // Then we clear the variables
-//     let second = 0,
-//       minute = 0,
-//       hour = 0;
-  
-//     // Next we set a interval every 1000 ms
-//     timerInterval = setInterval(function () {
-  
-//       // We set the timer text to include a two digit representation
-//       timer.innerHTML =
-//         (hour ? hour + ':' : '') +
-//         (minute < 10 ? '0' + minute : minute) +
-//         ':' +
-//         (second < 10 ? '0' + second : second);
-  
-//       // Next, we add a new second since one second is passed
-//       second++;
-  
-//       // We check if the second equals 60 "one minute"
-//       if (second == 60) {
-//         // If so, we add a minute and reset our seconds to 0
-//         minute++;
-//         second = 0;
-//       }
-  
-//       // If we hit 60 minutes "one hour" we reset the minutes and plus an hour
-//       if (minute === 03) {
-//         stop(timer);
-//         alert('Time\'s up! Let\'s tally your score!');
-//       }
-//     }, 1000);
-//   };
+
+
+
+// Have start button reveal questions
+// startButton.addEventListener('click', function () {
+//         let questions = document.getElementsByClassName(".questions")
+//         if (questions.style.visibility === "block") {
+//             questions.style.visibility === "hidden"
+//         }
+//         else {
+//             questions.style.visibility = "block"
+//         }
+//     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Configure Timer along with game start button
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
