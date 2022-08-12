3a016992ec7dfbb81e0ff28e03e18c4bc7762b09
RickHayden <richard.hayden459@live.com>
2022-08-12 03:41:24 -0400

    All question pages linked, polishing win conditions

--- app.js
+++ app.js
@@ -1,102 +1,26 @@
 //console.log('LINK: START')
 // grab elements from html
-let gameActive = true;
 let questions = document.querySelectorAll('.questions')
-let startButton = document.querySelector('#startButton')
 let answer = document.querySelectorAll('.answer')
-let nextButton = document.querySelector('.next')
-let part2 = document.querySelectorAll(".questions2")
-
-
-
-
-
-
-
-// questions[0].style.visibility = 'hidden';
-// questions[1].style.visibility = 'hidden';
-// questions[2].style.visibility = 'hidden';
-// questions[3].style.visibility = 'hidden';
-// questions[4].style.visibility = 'hidden';
-
-
-
-startButton.addEventListener('click', function(evt) {
-    questions.forEach(function(e) {
-        questions.style.display = 'block'
-    })
-    gameActive = true;
-})
-
-
-
-
-
-
-// Have start button reveal questions
-// startButton.addEventListener('click', function () {
-//         let questions = document.getElementsByClassName(".questions")
-//         if (questions.style.visibility === "block") {
-//             questions.style.visibility === "hidden"
-//         }
-//         else {
-//             questions.style.visibility = "block"
-//         }
-//     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Configure Timer along with game start button
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let submit = document.querySelector('.submit')
+
+// make submit button record and respond to answers from radio input
+function handleClick() {
+    const radioButtons = document.querySelectorAll('input[value= "4"]');
+    let selectedValue;
+    for (const rb of radioButtons) {
+    if (rb.checked) {
+    selectedValue = rb.value;
+    break;
+          }
+      }
+    if (selectedValue === '4')  {
+        alert('Great job! Press the "Next" button to move on to the next branch, climber!');
+      }
+      else if (selectedValue === '8') {
+        alert('Congratulations! you made it to the top of the tree. Press "next" to enjoy the view!');
+      }
+    else{
+        alert("Ahh, you almost had it... Maybe? Oh well. Try again!");
+    }
+    }
